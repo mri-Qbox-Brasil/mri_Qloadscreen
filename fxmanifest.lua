606305704014926ed1992e5bb5acf2ee45174996
mur4i <murainobeat@gmail.com>
2024-12-22 17:26:56 -0300

    fixed

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,11 +7,11 @@ description 'Simple Loading Screen'
 credits 'Beta Studio'
 
 lua54 'yes'
-version '2.0.3' 
+version '2.0.4' 
 
 loadscreen 'public/index.html'
 loadscreen_cursor 'yes'
-loadscreen_manual_shutdown 'false'
+loadscreen_manual_shutdown 'no'
 
 files {
 	'public/**',
